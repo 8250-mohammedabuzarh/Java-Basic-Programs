acfbd6ce6a7d725ef4def442b819296735457167
Mohammed Abuzarh <Mohammed.Abuzarh@godigit.com>
2024-07-25 22:10:08 +0530

    print default datatype values

---
+++ javabasics/Datatypevalues.java
@@ -0,0 +1,23 @@
+package javabasics;
+
+public class Datatypevalues {
+    int a;
+    char b;
+    double c;
+    boolean d;
+    short e;
+    long f;
+    float g;
+    public static void main(String[] abs) {
+        Datatypevalues obj = new Datatypevalues();
+        System.out.println("Default values of data types:");
+        System.out.println("int: " + obj.a);
+        System.out.println("char: " + obj.b);
+        System.out.println("double: " + obj.c);
+        System.out.println("boolean: " + obj.d);
+        System.out.println("short: " + obj.e);
+        System.out.println("long: " + obj.f);
+        System.out.println("float: " + obj.g);
+    }
+
+}
