b97e82e6422b898b2d13d1ae09c79538d51a1e48
Mohammed Abuzarh <Mohammed.Abuzarh@godigit.com>
2024-07-25 22:11:57 +0530

    Display message

---
+++ javabasics/Display.java
@@ -0,0 +1,11 @@
+package javabasics;
+
+public class Display {
+    public void display() {
+        System.out.println("Basic Java Program");
+    }
+    public static void main(String[] args) {
+        Display obj = new Display();
+        obj.display();
+    }
+}
